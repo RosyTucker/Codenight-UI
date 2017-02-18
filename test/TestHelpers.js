83d5b6a55cfcf52a2c9a13a11be27505dce17e18
Rose Tucker <rosy533@gmail.com>
2017-02-18 11:42:07 +0000

    Add success promise helper

--- test/TestHelpers.js
+++ test/TestHelpers.js
@@ -10,6 +10,27 @@ const { expect } = chai;
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const PromiseHelper = {
+  success(promiseObj, expectationsFunction, doneFunction) {
+    if (!promiseObj) {
+      throw new Error('Promise undefined');
+    }
+
+    new Promise((resolve, reject) => {
+      promiseObj.then(result => {
+        try {
+          expectationsFunction(result);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      }, () => {
+        reject('Promise Rejected');
+      });
+    }).then(doneFunction, doneFunction);
+  }
+};
+
 const Sandbox = () => sinon.sandbox.create();
 
 global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
@@ -20,5 +41,6 @@ export {
   Enzyme,
   expect,
   sinon,
+  PromiseHelper,
   Sandbox
 };
