6dbc91d48018c3bbe153749c19dd7cee2da1a69d
Rose Tucker <rosy533@gmail.com>
2017-02-19 11:24:39 +0000

    Prune scripts

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-rm -rf dist
-babel src/index.js --out-file dist/index.js
-
-webpack
--- scripts/buildProd.sh
+++ scripts/buildProd.sh
@@ -3,6 +3,12 @@ set -e
 NODE_ENV=production
 
 rm -rf dist
+mkdir dist
+mkdir dist/client
+
+cp -r src/client/font dist/client
+cp -r src/client/vendorStyles dist/client
+
 babel src/index.js --out-dir dist/index.js
 
 webpack -p
