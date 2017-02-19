de82533b3332160911c4843c374c7a904969818d
Rose Tucker <rosy533@gmail.com>
2017-02-19 12:49:13 +0000

    Deploy

--- scripts/deployHeroku.sh
+++ scripts/deployHeroku.sh
@@ -24,23 +24,8 @@ git commit -m "$COMMIT_MESSAGE"
 
 echo "Setting config vars"
 
-heroku config:set GITHUB_CLIENT_ID=$GITHUB_CLIENT_ID --app $APP_NAME &> /dev/null
-echo "Set GITHUB_CLIENT_ID"
-
-heroku config:set GITHUB_CLIENT_SECRET=$GITHUB_CLIENT_SECRET --app $APP_NAME &> /dev/null
-echo "Set GITHUB_CLIENT_SECRET"
-
 heroku config:set BASE_URL=$BASE_URL --app $APP_NAME &> /dev/null
 echo "Set BASE_URL"
-
-heroku config:set SESSION_SECRET=$SESSION_SECRET --app $APP_NAME &> /dev/null
-echo "Set SESSION_SECRET"
-
-heroku config:set REDIS_URL=$REDIS_URL --app $APP_NAME &> /dev/null
-echo "Set REDIS_URL"
-
-heroku config:set MONGODB_URI=$MONGODB_URI --app $APP_NAME &> /dev/null
-echo "Set MONGODB_URI"
 
 echo "Pushing to heroku"
 git push heroku master -f
